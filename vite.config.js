1051b3b93ffb7941125a7b8014afcf38f7c58e00
Cord Vigil <cv86096@email.vccs.edu>
2025-02-11 18:46:24 -0500

    Adding in RPG

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 CC: resolve(__dirname, 'CC/index.html'),
-                
+                RPG: resolve(__dirname, 'RPG/index.html'),
             }
         }
     }
