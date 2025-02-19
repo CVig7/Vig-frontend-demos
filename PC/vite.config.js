3bd84a067c06eb27a6e58229398323e7cec5eb15
Cord Vigil <cv86096@email.vccs.edu>
2025-02-19 02:14:04 -0500

    This should square things up

--- PC/vite.config.js
+++ PC/vite.config.js
@@ -3,7 +3,7 @@ import{ defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    base: "/Vig-pages/RPG/",
+    base: "/Vig-pages/PC/",
     build: {
         outDir: "docs",
         rollupOptions : {
@@ -17,4 +17,4 @@ export default defineConfig({
         }
     }
 });
-        
+   
