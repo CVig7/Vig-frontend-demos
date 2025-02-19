fd6b0f7b38f01590aa746807a146585c55539777
Cord Vigil <cv86096@email.vccs.edu>
2025-02-19 00:34:29 -0500

    Link fix

--- RPG/vite.config.js
+++ RPG/vite.config.js
@@ -3,7 +3,7 @@ import{ defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    base: "/Vig-pages/",
+    base: "/Vig-pages/ RPG/",
     build: {
         outDir: "docs",
         rollupOptions : {
