e4deed1086370b687b0674d29f29ddaf87d61df7
Cord Vigil <cv86096@email.vccs.edu>
2025-04-07 02:52:08 -0400

    Major updates to layout and added in scroll features with precision bar currently in the works, cant get the precision to work yet stay tuned.

--- docs/assets/main-BLxOg3gq.js
+++ docs/assets/main-BLxOg3gq.js
@@ -1,4 +1,4 @@
-import"./styles-BDVpRq4S.js";var Je=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},kt={exports:{}};/**
+import"./styles-BZSrQvHl.js";var Je=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},kt={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
