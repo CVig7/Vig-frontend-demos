ca473bdc7c82117bc4af29164e4e47825f65d386
Cord Vigil <cv86096@email.vccs.edu>
2025-04-07 00:49:57 -0400

    Got the global page going and all pages linked properly. Lets GO!

--- docs/assets/main-CsM-G9NC.js
+++ docs/assets/main-CsM-G9NC.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";var Je=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},kt={exports:{}};/**
+import"./styles-BDVpRq4S.js";var Je=typeof globalThis<"u"?globalThis:typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},kt={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
